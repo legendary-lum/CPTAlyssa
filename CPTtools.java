f8142d2c8512114c4ab4e5199398d0fd53b3fee7
legendary-lum <alyssa.lum26@ycdsbk12.ca>
2025-06-03 10:38:10 -0400

    added CPTtools to Github folder

---
+++ CPTtools.java
@@ -0,0 +1,23 @@
+import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+public class CPTtools{
+	public static void player1(String strP1){
+		String strP1;
+		int intX = 0;
+		int intY = 0;
+		
+		con.setDrawColor(Color.RED);
+		
+		if(strP1equals.ignoreCase("1")){
+		
+		intX = intX + 650;
+		intY = intY + 650;
+		}
+		con.fillOval(intX, intY, 100, 100);
+		
+		return
+	}
+
+}

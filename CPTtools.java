454d7cfe43bb5317f7bdd36da9ae368fb127f7e2
legendary-lum <alyssa.lum26@ycdsbk12.ca>
2025-06-04 09:44:06 -0400

    Improved gameplay

--- CPTtools.java
+++ CPTtools.java
@@ -4,18 +4,7 @@ import java.awt.image.BufferedImage;
 
 public class CPTtools{
 	public static void player1(String strP1){
-		String strP1;
-		int intX = 0;
-		int intY = 0;
-		
-		con.setDrawColor(Color.RED);
-		
-		if(strP1equals.ignoreCase("1")){
-		
-		intX = intX + 650;
-		intY = intY + 650;
-		}
-		con.fillOval(intX, intY, 100, 100);
+	
 		
 		return
 	}

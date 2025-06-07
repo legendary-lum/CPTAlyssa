a1d89994e851788c372b378c15508f879d8580a2
legendary-lum <alyssa.lum26@ycdsbk12.ca>
2025-06-07 19:00:54 -0400

    Checked array for vertical and horizontal matches

--- CPTtools.java
+++ CPTtools.java
@@ -13,14 +13,31 @@ public class CPTtools{
 		for(intRowCheck = 0; intRowCheck <= 5; intRowCheck++){
 			for(intColumnCheck = 0; intColumnCheck <= 3; intColumnCheck++){
 				if(intboard[intRowCheck][intColumnCheck] == 1 && intboard[intRowCheck][intColumnCheck+1] == 1 && intboard[intRowCheck][intColumnCheck+2] == 1 && intboard[intRowCheck][intColumnCheck+3] == 1){
-					intMatch[0][0] = 0;
+					intMatch[0][0] = intRowCheck;
 					intMatch[0][1] = intColumnCheck;
-					intMatch[1][0] = 0;
+					intMatch[1][0] = intRowCheck;
 					intMatch[1][1] = intColumnCheck+1;
-					intMatch[2][0] = 0;
+					intMatch[2][0] = intRowCheck;
 					intMatch[2][1] = intColumnCheck+2;
-					intMatch[3][0] = 0;
+					intMatch[3][0] = intRowCheck;
 					intMatch[3][1] = intColumnCheck+3;
+					blnMatch = true;
+				}
+			}
+		}
+	 
+		for(intRowCheck = 0; intRowCheck <= 5; intRowCheck++){
+			for(intColumnCheck = 0; intColumnCheck <= 3; intColumnCheck++){
+				if(intboard[intColumnCheck][intRowCheck] == 1 && intboard[intColumnCheck+1][intRowCheck] == 1 && intboard[intColumnCheck+2][intRowCheck] == 1 && intboard[intColumnCheck+3][intRowCheck] == 1){
+					intMatch[0][0] = intColumnCheck;
+					intMatch[0][1] = intRowCheck;
+					intMatch[1][0] = intColumnCheck;
+					intMatch[1][1] = intRowCheck+1;
+					intMatch[2][0] = intColumnCheck;
+					intMatch[2][1] = intRowCheck+2;
+					intMatch[3][0] = intColumnCheck;
+					intMatch[3][1] = intRowCheck+3;
+
 					blnMatch = true;
 				}
 			}
